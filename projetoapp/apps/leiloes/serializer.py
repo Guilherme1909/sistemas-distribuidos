2f663c8ae36456bada171e477f797ac18402f308
willyam <willyamramosdutra@gmail.com>
2025-06-20 18:50:48 -0300

    Adicionado função para verificar se item ja foi vendido

--- projetoapp/apps/leiloes/serializer.py
+++ projetoapp/apps/leiloes/serializer.py
@@ -5,12 +5,25 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
-        # Ou se quiser campos específicos:
-        # fields = ['id', 'created_on', 'updated_on', 'name', 'description']
+
 
 class LanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lance
         fields = '__all__'
-        # Ou se quiser campos específicos:
-        # fields = ['id', 'created_on', 'updated_on', 'name', 'description']
+    def validate_valor(self, valor):
+        item = self.initial_data.get('item')
+        if not item:
+            raise serializers.ValidationError("Item inválido.")
+
+        # Pega o objeto Item pelo ID para verificar valorFinal
+        try:
+            item_obj = Item.objects.get(pk=item)
+        except Item.DoesNotExist:
+            raise serializers.ValidationError("Item não encontrado.")
+
+        if item_obj.valorFinal is not None and valor > item_obj.valorFinal:
+            raise serializers.ValidationError(
+                f"O lance não pode ser maior que o valor máximo do item: {item_obj.valorFinal}"
+            )
+        return valor
